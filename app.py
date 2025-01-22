e9b7582eb660fa970a5ac842af6482eeb1a49e48
omjadhav007 <shirishkumar1949@gmail.com>
2025-01-22 10:28:44 +0530

    change in app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ load_dotenv()
 authenticator = Authenticator(
     token_key=os.getenv("TOKEN_KEY"),
     secret_path = os.path.join("etc","secrets","Bank_statement.json"),
-    redirect_uri="http://localhost:8501",
+    redirect_uri="https://autostatements.pro.onrender.com",
 )
 authenticator.check_auth()
 authenticator.login()
