35ca747864478e28ef993851ff104900e43ddff7
omjadhav007 <shirishkumar1949@gmail.com>
2025-02-18 22:39:58 +0530

    feature to edit categories

--- utils/util_functions.py
+++ utils/util_functions.py
@@ -12,17 +12,12 @@ import os
 import base64
 
 # Function to categorize each row based on narration content
-def categorize_debit_row(narration):
-    for key in debit_categorization_dict.keys():
+def categorize(narration,categorization_dict):
+    for key in categorization_dict.keys():
         if key.lower() in narration.lower():
-            return debit_categorization_dict[key]
+            return categorization_dict[key]
     return ""
 
-def categorize_credit_row(narration):
-    for key in credit_categorization_dict.keys():
-        if key.lower() in narration.lower():
-            return credit_categorization_dict[key]
-    return ""
 
 # Function to convert DataFrame to Excel
 def convert_df_to_excel(df):
@@ -100,9 +95,10 @@ def xlsx_to_df(uploaded_file):
     df = pd.read_excel(uploaded_file, engine='openpyxl')
     return df
 
-from utils import debit_categorization_dict,banks_date_format,table_columns_dic,table_columns_pdf_dic ,bank_status_dict, credit_categorization_dict
+from utils import banks_date_format,table_columns_dic,table_columns_pdf_dic ,bank_status_dict
+from database import get_category_df
 
-def format_uploaded_file(uploaded_file, bank):
+def format_uploaded_file(uploaded_file, bank, db_name, user_name):
     date_format=banks_date_format[bank]
     table_columns=table_columns_dic[bank]
     table_columns_pdf=table_columns_pdf_dic[bank]
@@ -190,13 +186,20 @@ def format_uploaded_file(uploaded_file, bank):
         # Add a new column 'Category'
         df['Category'] = ""
 
+        category_table=user_name if user_name!='professionalbuzz' and user_name!='shirishkumar1949' else "categories"
+        debit_categorization_df=get_category_df(db_name,category_table+"_debit")
+        credit_categorization_df=get_category_df(db_name,category_table+"_credit")
+
+        credit_categorization_dict = dict(zip(credit_categorization_df['keyword'], credit_categorization_df['category']))
+        debit_categorization_dict = dict(zip(debit_categorization_df['keyword'], debit_categorization_df['category']))
+
         df['Category'] = df.apply(
-            lambda row: categorize_credit_row(row['Narration']) if pd.notna(row['Credit']) and row['Credit'] != "" else "",
+            lambda row: categorize(row['Narration'],credit_categorization_dict) if pd.notna(row['Credit']) and row['Credit'] != "" else "",
             axis=1
         )
 
         df['Category'] = df.apply(
-            lambda row: categorize_debit_row(row['Narration']) if pd.notna(row['Debit']) and row['Debit'] != "" else row['Category'],
+            lambda row: categorize(row['Narration'],debit_categorization_dict) if pd.notna(row['Debit']) and row['Debit'] != "" else row['Category'],
             axis=1
         )
 
@@ -648,4 +651,8 @@ def show_agreement():
     """
     st.markdown(css_string, unsafe_allow_html=True)
     st.markdown(html_string, unsafe_allow_html=True)
+
+def has_common_rows(df1, df2):
+    common = pd.merge(df1, df2, how='inner')
+    return not common.empty
 
