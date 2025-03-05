78b477df51f1c360d07c6d480b7afe1a4a7c92f1
omjadhav007 <shirishkumar1949@gmail.com>
2025-03-05 19:36:50 +0530

    category ui changes

--- app.py
+++ app.py
@@ -696,7 +696,7 @@ if st.session_state["connected"]:
 
                 # Unique categories sorted A-Z
                 unique_categories = sorted(st.session_state.table_data["Category"].unique())
-                new_category = col[0].selectbox("Select Category", unique_categories + ["Add new category"], index=0)
+                new_category = col[0].selectbox("Select Category", ["Add new category"]+unique_categories, index=0)
 
                 if new_category == "Add new category":
                     new_category = col[1].text_input("Enter New Category")
