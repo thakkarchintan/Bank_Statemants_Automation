9c6608a4a048546e9e05340015abf558c12f9405
omjadhav007 <shirishkumar1949@gmail.com>
2025-03-06 22:11:36 +0530

    space instead of _ and reduced size of Category & Keyword Table

--- app.py
+++ app.py
@@ -514,7 +514,7 @@ if st.session_state["connected"]:
                 print(f"Error in showing transction data graph: {e}")
                 st.toast(":red[Something went wrong.]")
 
-        with tab3:    
+        with tab3:
             try:
                 if not db_df.empty:
                     db_df['Date'] = db_df['Date'].dt.strftime('%d-%b-%Y')
@@ -550,7 +550,17 @@ if st.session_state["connected"]:
                 if not db_df.empty:
                     # st.subheader("Summary")
                     if not summary_df.empty:
+
+                        # Rename columns
+                        old_col_names=["Start_Date","End_Date","Pending_days","Opening_balance","Closing_balance"]
+                        new_col_names=["Start Date","End Date","Pending days","Opening balance","Closing balance"]
+                        rename_dict = {o: n for o, n in zip(old_col_names, new_col_names) if o in summary_df.columns}
+                        summary_df=summary_df.rename(columns=rename_dict)
+
                         display_data(summary_df,200)
+
+                        rename_dict = {o: n for o, n in zip(new_col_names,old_col_names) if o in summary_df.columns}
+                        summary_df=summary_df.rename(columns=rename_dict)
 
                         # Convert the Date column to datetime and then format it
                         summary_df['Start_Date'] = pd.to_datetime(summary_df['Start_Date'],errors='coerce')
@@ -759,7 +769,7 @@ if st.session_state["connected"]:
                     refresh_table()
 
                 # Display the table
-                st.header("📋 Category & Keyword Table")
+                st.subheader("Category & Keyword Table")
 
                 gb = GridOptionsBuilder.from_dataframe(st.session_state.table_data)
 
